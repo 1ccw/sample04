b595c7ecd679abd221f735c8102e20ff936aeb8a
1ccw <140364678+1ccw@users.noreply.github.com>
2024-11-13 23:56:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ if (window.DeviceOrientationEvent) {
 
 // 서버로 데이터를 보내는 함수
 function sendDataToServer() {
-    fetch('http://fcda-175-125-49-71.ngrok-free.app/api/sensor-data', { // 서버 엔드포인트 URL로 교체
+    fetch('https://fcda-175-125-49-71.ngrok-free.app/api/sensor-data', { // 서버 엔드포인트 URL로 교체
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
